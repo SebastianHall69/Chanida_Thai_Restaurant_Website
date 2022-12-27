6e441d0063fdbcfbd76dc3b5952add7f3fc48ec4
SebastianHall69 <sebastianhall13@gmail.com>
2022-12-27 11:20:26 -0800

    Remove js files from gitignore

---
+++ src/site/js/legs.js
@@ -0,0 +1,7 @@
+"use strict";
+// Check script runs
+const people = ['James', 'Sebastian'];
+people.forEach((name) => {
+    console.log(name + ' has absolutely built forearms');
+});
+//# sourceMappingURL=legs.js.map
