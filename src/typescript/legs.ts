c76e3a3312ae55f987498edc80803ff6fb0b4688
SebastianHall69 <sebastianhall13@gmail.com>
2022-12-27 09:42:39 -0800

    Push changes to setup AWS Amplify

--- src/typescript/legs.ts
+++ src/typescript/legs.ts
@@ -1,14 +1,5 @@
+// Check script runs
 const people = ['James', 'Sebastian']
-const check = 'hello ' + 'world'
-const a = 7
-
-if (check === 'hello world') {
-  people.forEach((name) => {
-    console.log(name + ' has built wrists')
-  })
-} else {
-  console.log('screwed legs with very tense unhealthy muscles')
-}
-
-console.log('yi lan ma')
-console.log('ftx')
+people.forEach((name) => {
+  console.log(name + ' has absolutely built forearms')
+})
